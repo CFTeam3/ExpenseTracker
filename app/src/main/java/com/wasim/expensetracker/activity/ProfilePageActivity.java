6e9b48ada0d7fd27f12e32823136bfb6653d716d
Nicholas Brown <119603466+NicholasBrown-01@users.noreply.github.com>
2023-09-11 16:56:12 -0500

    Nick Brown: Pushing Android Studios Basic View Templates, XMLs, and Login/SignUp Button navigate correctly. Updated Android Manifest.xml & strings.xml for matching values.

---
+++ app/src/main/java/com/wasim/expensetracker/activity/ProfilePageActivity.java
@@ -0,0 +1,16 @@
+package com.wasim.expensetracker.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.wasim.expensetracker.R;
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
