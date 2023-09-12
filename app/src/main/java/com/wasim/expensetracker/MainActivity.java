675fc71ee29f933e65afe2c0188ef4c859b17070
Nicholas Brown <119603466+NicholasBrown-01@users.noreply.github.com>
2023-09-11 19:27:47 -0500

    Nick Brown: Updated and linked more buttons. SignUpNow button on SignUpActivity page needs troubleshooting with Cognito password requirements

--- app/src/main/java/com/wasim/expensetracker/MainActivity.java
+++ app/src/main/java/com/wasim/expensetracker/MainActivity.java
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+
 
 import com.wasim.expensetracker.activity.ProfilePageActivity;
 import com.wasim.expensetracker.activity.SignUpActivity;
@@ -19,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button signupButton = findViewById(R.id.MainActivitySignUpButton);
 
         setupLoginButton();
         setupSignUpButton();
--- app/src/main/java/com/wasim/expensetracker/activity/SignUpActivity.java
+++ app/src/main/java/com/wasim/expensetracker/activity/SignUpActivity.java
@@ -1,16 +1,56 @@
 package com.wasim.expensetracker.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+import com.wasim.expensetracker.MainActivity;
 import com.wasim.expensetracker.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    private final String TAG = "SIGNUP ACTIVITY";
+
+    Button signUpNowButton;
+
+    EditText usernameEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpNowButton = findViewById(R.id.SignUpActivitySignUpButton);
+        usernameEditText = findViewById(R.id.SignUpActivityCreateUsernamePlainText);
+        passwordEditText = findViewById(R.id.SignUpActivityCreatePasswordTextView);
+
+        setupSignUpNowButton();
+    }
+
+    void setupSignUpNowButton() {
+
+        signUpNowButton.setOnClickListener(view -> {
+
+            Amplify.Auth.signUp(usernameEditText.getText().toString(),
+                    passwordEditText.getText().toString(),
+                    AuthSignUpOptions.builder()
+                            .userAttribute(AuthUserAttributeKey.preferredUsername(), usernameEditText.getText().toString())
+                            .build(),
+                    successResponse -> {
+                        Log.i(TAG, "Sign Up SUCCESSFULL" + successResponse);
+                        Intent goToProfilePageActivity = new Intent(SignUpActivity.this, ProfilePageActivity.class);
+                        startActivity(goToProfilePageActivity);
+                    },
+                    failureResponse -> Log.i(TAG, "Sign Up FAILED" + failureResponse)
+            );
+        });
+
     }
 }
